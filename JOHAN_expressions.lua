18b731de6f1408d03d700ec2adea4a290f5b3963
abbliseng <54662679+abbliseng@users.noreply.github.com>
2025-07-15 16:26:07 +0200

    Sync from VS Code

--- JOHAN_expressions.lua
+++ JOHAN_expressions.lua
@@ -1,7 +1,11 @@
 -- print out all redstone relay peripherals
 local peripherals = peripheral.getNames()
--- sort the peripherals alphabetically
-table.sort(peripherals)
+-- sort the peripherals by their last number
+table.sort(peripherals, function(a, b)
+    local aNum = tonumber(string.match(a, "%d+$"))
+    local bNum = tonumber(string.match(b, "%d+$"))
+    return aNum < bNum
+end)
 for _, name in ipairs(peripherals) do
     if peripheral.getType(name) == "redstone_relay" then
         print(name)
