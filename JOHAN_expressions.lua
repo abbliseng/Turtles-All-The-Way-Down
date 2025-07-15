2e35f11ef7e64592b5635664a56967b8048ddeda
abbliseng <54662679+abbliseng@users.noreply.github.com>
2025-07-15 16:27:00 +0200

    Sync from VS Code

--- JOHAN_expressions.lua
+++ JOHAN_expressions.lua
@@ -1,13 +1,12 @@
 -- print out all redstone relay peripherals
 local peripherals = peripheral.getNames()
--- sort the peripherals by their last number
-table.sort(peripherals, function(a, b)
-    local aNum = tonumber(string.match(a, "%d+$"))
-    local bNum = tonumber(string.match(b, "%d+$"))
-    return aNum < bNum
-end)
+local count = 0
 for _, name in ipairs(peripherals) do
     if peripheral.getType(name) == "redstone_relay" then
+        count = count + 1
         print(name)
     end
 end
+
+-- print out the total number of redstone relays
+print("Total redstone relays: " .. count)
