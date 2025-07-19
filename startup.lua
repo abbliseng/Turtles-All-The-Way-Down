ccafaefa3a4aa9f05c570cd8e6b6e107e3da5bf7
abbliseng <54662679+abbliseng@users.noreply.github.com>
2025-07-19 02:27:00 +0200

    Sync from VS Code

--- startup.lua
+++ startup.lua
@@ -1,4 +1,8 @@
 -- startup.lua
 print("Starting Turtles All The Way Down...")
 shell.run("sync.lua")
+-- if the JOHAN startup script is present, run it
+if fs.exists("JOHAN/startup.lua") then
+    shell.run("JOHAN/startup.lua")
+end
 print("Ready to go!")
