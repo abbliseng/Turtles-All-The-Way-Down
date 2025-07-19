52703c39e0b5d697d3ae7b4c9950346bf2b51374
abbliseng <54662679+abbliseng@users.noreply.github.com>
2025-07-19 19:55:30 +0200

    Sync from VS Code

--- JOHAN/startup.lua
+++ JOHAN/startup.lua
@@ -1,12 +1,14 @@
 print("JOHANS startup...")
 if fs.exists("JOHAN/consciousness.lua") then
-    shell.run("JOHAN/consciousness.lua")
+    -- shell.run("JOHAN/consciousness.lua")
+    multishell.launch({shell = shell}, "JOHAN/consciousness.lua")
 else
     print("JOHAN/consciousness.lua not found, skipping...")
 end
 
 if fs.exists("JOHAN/monitor.lua") then
-    shell.run("JOHAN/monitor.lua")
+    -- shell.run("JOHAN/monitor.lua")
+    multishell.launch({shell = shell}, "JOHAN/monitor.lua")
 else
     print("JOHAN/monitor.lua not found, skipping...")
 end
