ee9866f5d4877e0f966811578f1b23d8f79ce898
abbliseng <54662679+abbliseng@users.noreply.github.com>
2025-07-23 18:21:25 +0200

    Sync from VS Code

--- JOHAN/ae/bees_reciever.lua
+++ JOHAN/ae/bees_reciever.lua
@@ -1,15 +1,16 @@
 rednet.open("back")
 
-local function receiveAvailableItems()
+local function receiveAvailableItems(items)
     print("Synced at " .. os.date())
     local file = fs.open("JOHAN/ae/data/bees_available_items.txt", "w")
-    file.write(message)
+    print()
+    file.write(items)
     file.close()
 end
 
 while true do
     local event, sender, message, protocol = os.pullEvent("rednet_message")
     if sender == 6 then
-        receiveAvailableItems()
+        receiveAvailableItems(message)
     end
 end
