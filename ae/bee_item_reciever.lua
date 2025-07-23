a0209d0da83e934926f3fdb81edcf22170d666d1
abbliseng <54662679+abbliseng@users.noreply.github.com>
2025-07-23 21:56:37 +0200

    Sync from VS Code

--- ae/bee_item_reciever.lua
+++ ae/bee_item_reciever.lua
@@ -2,13 +2,12 @@ rednet.open("top")
 local ae_reader = peripheral.wrap("me_bridge_3")
 
 
--- while true do
+while true do
     local event, sender, message, protocol = os.pullEvent("rednet_message")
     if sender == 4 then
-        print(message)
         message = textutils.unserialize(message)
         for key, item in pairs(message) do
             ae_reader.exportItem(item, "down")
         end
     end
--- end
+end
