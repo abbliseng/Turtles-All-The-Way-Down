b1da174c968f68d255a15861bfa2e92c96664396
abbliseng <54662679+abbliseng@users.noreply.github.com>
2025-07-23 15:03:38 +0200

    Sync from VS Code

--- ae/bee_reciever.lua
+++ ae/bee_reciever.lua
@@ -1,3 +1,17 @@
-rednet.open("top")
-local id, message = rednet.receive()
-print("Received message from ID " .. id .. ": " .. message)
+local get_available_items_timer = os.startTimer(5)
+ae_reader = peripheral.wrap("me_bridge_2")
+
+items = ae_reader.getItems()
+print("Items: " .. #items)
+
+-- rednet.open("top")
+-- local id, message = rednet.receive()
+-- print("Received message from ID " .. id .. ": " .. message)
+
+-- while true do
+--     local event, param = os.pullEvent()
+--     if event == "timer" and param == get_available_items_timer then
+--         -- rednet.send(6, "get_available_items")
+--         get_available_items_timer = os.startTimer(5)
+--     end
+-- end
