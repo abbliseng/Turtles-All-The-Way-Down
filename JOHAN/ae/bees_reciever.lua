b0c2a3953dc0d5469a0a9209ce8a081644cb118b
abbliseng <54662679+abbliseng@users.noreply.github.com>
2025-07-23 18:54:34 +0200

    Sync from VS Code

--- JOHAN/ae/bees_reciever.lua
+++ JOHAN/ae/bees_reciever.lua
@@ -3,12 +3,7 @@ rednet.open("back")
 local function receiveAvailableItems(items)
     print("Synced at " .. os.date())
     local file = fs.open("JOHAN/ae/data/bees_available_items.txt", "w")
-    for key, item in ipairs(items) do
-        file.write(item)
-        if key < #items then
-            file.write("\n")  -- Add a newline after each item except the last one
-        end
-    end
+    file.write(items)
     file.close()
 end
 
