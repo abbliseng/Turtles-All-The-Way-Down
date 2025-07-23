66ab23eeb455270e6a65f0798b5086f0aa2e4f9f
abbliseng <54662679+abbliseng@users.noreply.github.com>
2025-07-23 21:55:41 +0200

    Sync from VS Code

--- ae/bee_item_reciever.lua
+++ ae/bee_item_reciever.lua
@@ -8,8 +8,7 @@ local ae_reader = peripheral.wrap("me_bridge_3")
         print(message)
         message = textutils.unserialize(message)
         for key, item in pairs(message) do
-            -- print(item)
-            -- ae_reader.exportItem(item, "")
+            ae_reader.exportItem(item, "down")
         end
     end
 -- end
